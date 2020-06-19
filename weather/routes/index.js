2d8d3b55eda597102f50edbed95bbce1125cff58
Roland Mueller <roland.muller@nokia.com>
2020-06-19 15:42:31 +0300

    logging added

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -12,8 +12,8 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
-
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&APPID=${OWM_API_KEY}`;
+  console.info(url);
   try {
     let data = await fetch(url);
     let weather = await data.json();
